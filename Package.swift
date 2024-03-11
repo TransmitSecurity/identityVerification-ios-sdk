adff477a6dda4b5f90dfdc9a1f12c5034775ddd4
Igor Babitski <igor.babitski@transmitsecurity.com>
2024-03-11 11:56:37 +0200

    New SDK version 1.0.13

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["IdentityVerification", "IdentityVerification-Dependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.19"),
-        .package(url: "https://github.com/TransmitSecurity/accountprotection-ios-sdk.git", from: "2.0.7")
+        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.20"),
+        .package(url: "https://github.com/TransmitSecurity/accountprotection-ios-sdk.git", from: "2.0.8")
     ],
     targets: [
         .binaryTarget(
