2524fdf8b8a1a7c60594e9c3b3aa5f4d2445879f
Tomer Picker <>
2025-05-31 13:27:03 +0300

    Release 1.2.5 Updated files structure

--- build/IdentityVerification.xcframework/Docs/Build/Intermediates.noindex/IdentityVerification.build/Debug-iphoneos/IdentityVerification.build/DerivedSources/IdentityVerification_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char IdentityVerificationVersionString[];
- extern const double IdentityVerificationVersionNumber;
-
- const unsigned char IdentityVerificationVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:IdentityVerification  PROJECT:IdentityVerification-1.2.5" "\n";
- const double IdentityVerificationVersionNumber __attribute__ ((used)) = (double)1.2;
--- build/IdentityVerification.xcframework/Docs/Build/Intermediates.noindex/IdentityVerification.build/Debug-iphoneos/IdentityVerification.build/Objects-normal/arm64/IdentityVerification_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ec2-user/actions-runner/_work/verifyid-ios-demo-app/verifyid-ios-demo-app/verifyid-ios-demo-app/Docs/Build/Intermediates.noindex/IdentityVerification.build/Debug-iphoneos/IdentityVerification.build/module.modulemap \
-  /Users/ec2-user/actions-runner/_work/verifyid-ios-demo-app/verifyid-ios-demo-app/verifyid-ios-demo-app/Docs/Build/Intermediates.noindex/IdentityVerification.build/Debug-iphoneos/IdentityVerification.build/DerivedSources/IdentityVerification_vers.c
