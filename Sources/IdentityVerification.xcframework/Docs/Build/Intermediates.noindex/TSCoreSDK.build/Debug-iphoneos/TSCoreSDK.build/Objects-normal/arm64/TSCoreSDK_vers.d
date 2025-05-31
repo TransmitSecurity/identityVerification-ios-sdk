c0b16109a9f6005737ecc9ce9bff74fb758a394e
Igor Babitski <igor.babitski@transmitsecurity.com>
2025-06-01 00:11:02 +0300

    Unused files removed

--- Sources/IdentityVerification.xcframework/Docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/Objects-normal/arm64/TSCoreSDK_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ec2-user/actions-runner/_work/verifyid-ios-demo-app/verifyid-ios-demo-app/verifyid-ios-demo-app/Docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/module.modulemap \
-  /Users/ec2-user/actions-runner/_work/verifyid-ios-demo-app/verifyid-ios-demo-app/verifyid-ios-demo-app/Docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/DerivedSources/TSCoreSDK_vers.c
